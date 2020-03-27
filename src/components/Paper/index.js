29b0574aef35c75fb26b21a5526a07d06b28e01e
Marcus Correa <mrc.correa@gmail.com>
2020-03-26 21:49:05 -0300

    Extended classes prop inside Paper component.

--- src/components/Paper/index.js
+++ src/components/Paper/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 import UIPaper from '@material-ui/core/Paper'
 import {makeStyles} from '@material-ui/core/styles'
 
-const Paper = ({children, inset}) => {
+const Paper = ({children, inset, classes, ...props}) => {
   const boxShadow = inset ? 'inset 15px 15px 11px -14px rgba(196,196,196,1)' : '0px 0px 15px 5px rgba(227,227,227,1)'
 
   const useStyles = makeStyles(() => ({
@@ -12,20 +12,27 @@ const Paper = ({children, inset}) => {
       borderRadius: '10px',
       boxShadow: boxShadow,
     },
+    ...classes,
   }))
 
-  const classes = useStyles()
+  const paperClasses = useStyles()
 
-  return <UIPaper classes={classes}>{children}</UIPaper>
+  return (
+    <UIPaper {...props} classes={paperClasses}>
+      {children}
+    </UIPaper>
+  )
 }
 
 Paper.propTypes = {
-  children: PropTypes.any,
+  children: PropTypes.any.isRequired,
   inset: PropTypes.bool,
+  classes: PropTypes.object,
 }
 
 Paper.defaultProps = {
   inset: false,
+  classes: {},
 }
 
 export default Paper
