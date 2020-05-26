f3ef9634fced06de59dcf02fea68012cf16a56f4
Marcus Correa <mrc.correa@gmail.com>
2020-05-25 22:30:35 -0300

    Added styles to responsive table.

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import clsx from 'clsx'
 
 import {Table, Thead, Tbody, Tr, Th, Td} from 'react-super-responsive-table'
-import {Paper} from 'components'
 
-import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+import './SuperResponsiveTableStyle.css'
 
 import styles from './index.module.scss'
 
@@ -11,54 +11,52 @@ import styles from './index.module.scss'
 
 const RSRTable = () => {
   return (
-    <Paper inset>
-      <Table className={styles.table}>
-        <Thead>
-          <Tr>
-            <Th>Event</Th>
-            <Th>Date</Th>
-            <Th>Location</Th>
-            <Th>Edit</Th>
-            <Th>Delete</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td>Tablescon</Td>
-            <Td>9 April 2019</Td>
-            <Td>East Annex</Td>
-            <Td>
-              <i className='far fa-edit' />
-            </Td>
-            <Td>
-              <i className='far fa-trash-alt' />
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>Capstone Data</Td>
-            <Td>19 May 2019</Td>
-            <Td>205 Gorgas</Td>
-            <Td>
-              <i className='far fa-edit' />
-            </Td>
-            <Td>
-              <i className='far fa-trash-alt' />
-            </Td>
-          </Tr>
-          <Tr>
-            <Td>Tuscaloosa D3</Td>
-            <Td>29 June 2019</Td>
-            <Td>Github</Td>
-            <Td>
-              <i className='far fa-edit' />
-            </Td>
-            <Td>
-              <i className='far fa-trash-alt' />
-            </Td>
-          </Tr>
-        </Tbody>
-      </Table>
-    </Paper>
+    <Table className={clsx(styles.table, styles.responsiveTable)}>
+      <Thead>
+        <Tr>
+          <Th>Event</Th>
+          <Th>Date</Th>
+          <Th>Location</Th>
+          <Th>Edit</Th>
+          <Th>Delete</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        <Tr>
+          <Td>Tablescon</Td>
+          <Td>9 April 2019</Td>
+          <Td>East Annex</Td>
+          <Td className={styles.icon}>
+            <i className='far fa-edit' />
+          </Td>
+          <Td className={styles.icon}>
+            <i className='far fa-trash-alt' />
+          </Td>
+        </Tr>
+        <Tr>
+          <Td>Capstone Data</Td>
+          <Td>19 May 2019</Td>
+          <Td>205 Gorgas</Td>
+          <Td className={styles.icon}>
+            <i className='far fa-edit' />
+          </Td>
+          <Td className={styles.icon}>
+            <i className='far fa-trash-alt' />
+          </Td>
+        </Tr>
+        <Tr>
+          <Td>Tuscaloosa D3</Td>
+          <Td>29 June 2019</Td>
+          <Td>Github</Td>
+          <Td className={styles.icon}>
+            <i className='far fa-edit' />
+          </Td>
+          <Td className={styles.icon}>
+            <i className='far fa-trash-alt' />
+          </Td>
+        </Tr>
+      </Tbody>
+    </Table>
   )
 }
 
--- src/pages/ClientList/index.js
+++ src/pages/ClientList/index.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+
+import {Paper, Table} from 'components'
+
+import {useStore} from 'store'
+
+import styles from './index.module.scss'
 
 const ClientList = () => {
-  return <div>client list</div>
+  const store = useStore()
+  useEffect(() => {
+    store.showMenu()
+  }, [store])
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Clientes</h1>
+      </header>
+      <Paper className={styles.paper}>
+        <Table />
+      </Paper>
+    </>
+  )
 }
 
 export default ClientList
