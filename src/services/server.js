c98b2929c6e38705cc527b787efeeebb0c91a5a8
Marcus Correa <mrc.correa@gmail.com>
2021-05-19 18:54:36 -0300

    Changed server to be production.

--- src/services/server.js
+++ src/services/server.js
@@ -1,3 +1,3 @@
 // export default 'http://a077488f3d98.ngrok.io/api/v1'
-export default 'http://localhost:8000/api/v1'
-// export default 'https://comandasmart.com.br/api/v1'
+// export default 'http://localhost:8000/api/v1'
+export default 'https://comandasmart.com.br/api/v1'
